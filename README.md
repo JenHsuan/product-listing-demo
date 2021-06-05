2ef81046101beca17e0ee77fe77afaed083f49a5
JenHsuan <ofalpha@gmail.com>
2021-06-05 12:57:32 +0800

    improve the head of the page

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 It's a demo website for the product listing which is made by Next.js, React, and Styled-Components
 ### Keywords
 Next.js, React, Styled-Components, e-commerce
+
+## Demo
+* https://product-listing-demo-seanhsieh.netlify.app/
 ### Usages
 
 * Installation
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import Head from 'next/head';
 import ItemCellPage from '../components/ItemCellPage';
 
+import dog1 from '../public/images/dog/dog1.jpg';
 
 export default function index() {
+  const title = "e-commerce Product Listing Demo Site";
+  const description = "It's a demo website for the product listing which is made by Next.js, React, and Styled-Components";
   return (
     <>
       <Head>
-        <title>e-commerce Product Listing Demo Site</title>
-        <meta name="description" content="It's a demo website for the product listing which is made by Next.js, React, and Styled-Components" />
+        <title>{title }</title>
+        <meta name="description" content={description} />
         <meta name="keywords" content="Next.js, React, Styled-Components, e-commerce" />
         <meta name="author" content="Jen-Hsuan Hsieh (Sean Hsieh)"/>
+        <meta property="og:title" content={title}/>
+        <meta property="og:url" content="https://product-listing-demo-seanhsieh.netlify.app/"/>
+        <meta property="og:image" content={dog1}/>
+        <meta property="og:description" content={description}/>
       </Head>
       <ItemCellPage/>
     </>
